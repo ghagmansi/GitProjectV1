c396726d14a1fe6b5ece870a6924b0ad1f54c27e
Mansi Ghag <mansighag99@gmail.com>
2026-01-30 23:13:32 +0530

    Initial commit

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String action = "consistency";
+		
+		if(action == "consistency") {
+			System.out.println("Will achieve success!!");
+		} else {
+			System.out.println("Will delay your success...");
+		}
+
+	}
+
+}
