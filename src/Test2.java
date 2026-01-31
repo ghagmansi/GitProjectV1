88bb97f6b179c81dfbcf7093ac1efe8c594aa9c4
Mansi Ghag <mansi9ghag9@gmail.com>
2026-01-31 22:38:52 +0530

    Added additional checks in Test2

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		String action = "consistency";
 		
-		if(action == "consistency") {
+		if(action == "consistency" || action == "c" || action == "C") {
 			System.out.println("Test2 Will achieve success!!");
 		} else {
 			System.out.println("Test2 Will delay your success...");
